4b9ce2ec7e6e62733df83daee5beaaccc2976b62
GabrielMatiasn10 <gm4037003@gmail.com>
2024-02-01 15:27:29 -0300

    Inicio

---
+++ src/App.styles.ts
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+
+`;
+
+export const Area  = styled.div``;
+
+export const Header  = styled.div``;
+
+export const HeaderText = styled.div``;
+
+export const Body  = styled.div``;
