92f1ff1a93be651a7c4ae1ccaac23b0f5c2149bb
GabrielMatiasn10 <gm4037003@gmail.com>
2024-02-02 16:39:25 -0300

    Resolve the Problems

--- src/components/TableArea/index.tsx
+++ src/components/TableArea/index.tsx
@@ -1,36 +1,27 @@
-import * as C from './styles';  
+import * as C from './styles';
+import { Item } from '../../types/Item';
+import { TableItem } from '../TableItem';
 
-export const TableArea = () => {
+type Props = {
+    list: Item[]
+}
+
+export const TableArea = ({ list }: Props) => {
     return (
         <C.Table>
-        <thead>
-            <tr>
-            <C.TableHeadColumn width={100}>Data</C.TableHeadColumn>
-            <C.TableHeadColumn width={130}>Categoria</C.TableHeadColumn>
-            <C.TableHeadColumn>Título</C.TableHeadColumn>
-            <C.TableHeadColumn width={150}>Valor</C.TableHeadColumn>
-            </tr>
-        </thead>
-        <tbody>
-            <tr>
-            <td>15/09/2021</td>
-            <td>Alimentação</td>
-            <td>McDonalds</td>
-            <td>R$ 25,00</td>
-            </tr>
-            <tr>
-            <td>15/09/2021</td>
-            <td>Alimentação</td>
-            <td>Burger King</td>
-            <td>R$ 30,00</td>
-            </tr>
-            <tr>
-            <td>15/09/2021</td>
-            <td>Alimentação</td>
-            <td>Subway</td>
-            <td>R$ 20,00</td>
-            </tr>
-        </tbody>
+            <thead>
+                <tr>
+                    <C.TableHeadColumn width={100}>Data</C.TableHeadColumn>
+                    <C.TableHeadColumn width={130}>Categoria</C.TableHeadColumn>
+                    <C.TableHeadColumn>Título</C.TableHeadColumn>
+                    <C.TableHeadColumn width={150}>Valor</C.TableHeadColumn>
+                </tr>
+            </thead>
+            <tbody>
+                {list.map((item, index)=>(
+                    <TableItem key={index} item={item} />
+                ))}
+            </tbody>
         </C.Table>
-    )
-    }
+    );
+}
