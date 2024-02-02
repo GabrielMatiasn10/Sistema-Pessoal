8dc497fd092b9c130a1b76c5315edf938973a3ae
GabrielMatiasn10 <gm4037003@gmail.com>
2024-02-02 11:52:33 -0300

    Ending Categories

---
+++ src/data/categories.ts
@@ -0,0 +1,7 @@
+import { Category } from '../types/Category';
+
+export const categories: Category[] = [
+  { title: 'Alimentação', color: 'blue', expense: true },
+  { title: 'Aluguel', color: 'green', expense: true },
+  { title: 'Salário', color: 'red', expense: false }
+];
---
+++ src/types/Category.ts
@@ -0,0 +1,7 @@
+export type Category = {
+   
+    title: string;
+    color: string;
+    expense: boolean;
+   
+};
