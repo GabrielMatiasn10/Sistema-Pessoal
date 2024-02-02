c7761f7d467e06127ec540daa689e01d7d351be1
GabrielMatiasn10 <gm4037003@gmail.com>
2024-02-02 12:39:23 -0300

    Creating Tabela de Itens

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,19 @@
+import { useState, useEffect } from 'react'
 import * as C from './App.styles'
-import React from 'react';
+import { Item } from './types/Item'
+import { Category } from './types/Category'
+import { categories } from './data/categories'
+import { items } from './data/items'
+import { getCurrentMonth, filterListByMonth} from './helpers/data.filter'
+
 const App = () => {
+  const [list, setList] = useState(items);
+  const [ filteredList, setFilteredList ] = useState<Item[]>([]);
+  const [ currentMonth, setCurrentMonth ] = useState(getCurrentMonth());
+
+  useEffect(() => {
+    setFilteredList(filterListByMonth(list, currentMonth));
+  }, [list, currentMonth]);
   return (
     <C.Container>
       <C.Area>
@@ -10,7 +23,15 @@ const App = () => {
           </C.HeaderText>
          </C.Header>
         <C.Body>
-          ...
+          
+        { /* Área de informações */}
+
+        { /* Área de inserção */}
+
+        { /* Tabela de itens */}
+
+        { /* Rodapé */}
+
         </C.Body>
       </C.Area>
     </C.Container>
